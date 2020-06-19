b8976bcbaf21abeeee6a3bbbbcc0aa53e5a9eeda
SingletonSean <bballheat6@gmail.com>
2020-06-18 20:23:54 -0400

    Add server and client connection

---
+++ ColorChat.WPF/Commands/SendColorChatColorMessageCommand.cs
@@ -0,0 +1,49 @@
+﻿using ColorChat.Domain.Models;
+using ColorChat.WPF.Services;
+using ColorChat.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Windows.Input;
+
+namespace ColorChat.WPF.Commands
+{
+    public class SendColorChatColorMessageCommand : ICommand
+    {
+        private readonly ColorChatViewModel _viewModel;
+        private readonly SignalRChatService _chatService;
+
+        public SendColorChatColorMessageCommand(ColorChatViewModel viewModel, SignalRChatService chatService)
+        {
+            _viewModel = viewModel;
+            _chatService = chatService;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            try
+            {
+                await _chatService.SendColorMessage(new ColorChatColor()
+                {
+                    Red = _viewModel.Red,
+                    Green = _viewModel.Green,
+                    Blue = _viewModel.Blue,
+                });
+
+                _viewModel.ErrorMessage = string.Empty;
+            }
+            catch (Exception)
+            {
+                _viewModel.ErrorMessage = "Unable to send color message.";
+            }
+        }
+    }
+}
---
+++ ColorChat.WPF/Services/SignalRChatService.cs
@@ -0,0 +1,33 @@
+﻿using ColorChat.Domain.Models;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorChat.WPF.Services
+{
+    public class SignalRChatService
+    {
+        private readonly HubConnection _connection;
+
+        public event Action<ColorChatColor> ColorMessageReceived;
+
+        public SignalRChatService(HubConnection connection)
+        {
+            _connection = connection;
+
+            _connection.On<ColorChatColor>("ReceiveColorMessage", (color) => ColorMessageReceived?.Invoke(color));
+        }
+
+        public async Task Connect()
+        {
+            await _connection.StartAsync();
+        }
+
+        public async Task SendColorMessage(ColorChatColor color)
+        {
+            await _connection.SendAsync("SendColorMessage", color);
+        }
+    }
+}
